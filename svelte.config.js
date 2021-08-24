9f6c3f5c7d597fd097455e16ebec07bb11b68482
Shawn Shawn <laazy.pr00gramming@protonmail.com>
2021-08-24 22:19:17 +0800

    trying to fix svelte.config.js (i hope it fixes)

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
-import netlify from '@sveltejs/adapter-nelify'
+import adapter from '@sveltejs/adapter-nelify'
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-		adapter: netlify()
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
 
